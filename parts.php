386db4a16f82b905bf3af631d35001a83bee884b
Hijazi2003 <121342004+Hijazi2003@users.noreply.github.com>
2024-02-07 16:15:18 +0300

    Add files via upload

---
+++ parts.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "product";
+$password = "";
+$dbname = "try";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Collect values from the form
+    $productName = $_POST['productName'];
+    $productPrice = $_POST['productPrice'];
+    $productColor = $_POST['productColor'];
+    $catID = $_POST['CategoryID'];
+
+    $sqlquery = "INSERT INTO products (Pname, price,color , CategoryID) VALUES ('$productName' ,'$ProductPrice','$ProductColor','$ProducCategoryID' )";
+        
+    if ($conn->query($sqlquery) == TRUE) {
+        echo "record inserted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }  
+
+}
+
+?>
